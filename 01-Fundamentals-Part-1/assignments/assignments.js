4cb9640d46ae2ba5836708525eb6bb78e1ef6048
AleksManolow <amanolov7@gmail.com>
2025-01-29 12:08:46 +0200

    Add task Data Types

--- 01-Fundamentals-Part-1/assignments/assignments.js
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -6,3 +6,12 @@ let population  = 7;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Data Types
+let isIsland  = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
