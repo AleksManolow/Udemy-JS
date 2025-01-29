c145b893eafef5ebd0dd3e108245e326033cbda3
AleksManolow <amanolov7@gmail.com>
2025-01-29 12:40:54 +0200

    Add new tasks

--- 01-Fundamentals-Part-1/assignments/assignments.js
+++ 01-Fundamentals-Part-1/assignments/assignments.js
@@ -15,3 +15,21 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+//Let, const and var
+language = 'bulgarian';
+const yearOfFoundingOfTheState = 681;
+//yearOfFoundingOfTheState = 1978; - error!!
+
+//Basic Operators
+console.log(population / 2);
+
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+let description1  = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+
+console.log(description1);
