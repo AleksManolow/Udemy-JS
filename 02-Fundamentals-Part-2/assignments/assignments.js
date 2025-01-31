168c4c4870df1ca2a3075ff1f136a601fceb3beb
AleksManolow <amanolov7@gmail.com>
2025-01-31 09:27:06 +0200

    Create new assignments

--- 02-Fundamentals-Part-2/assignments/assignments.js
+++ 02-Fundamentals-Part-2/assignments/assignments.js
@@ -44,3 +44,37 @@ describePopulation('China', 1441);
 describePopulation('USA', 332);
 
 //Introduction to Arrays
+const populations = [10, 1441, 332, 83];
+
+console.log(populations.length === 4);
+
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3])
+];
+
+console.log(percentages);
+
+//Basic Array Operations (Methods)
+let neighbours = ['Norway', 'Sweden', 'Russia'];
+
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+neighbours.unshift('Utopia');
+console.log(neighbours);
+
+neighbours.shift();
+console.log(neighbours);
+
+if (!neighbours.includes('Germany')) {
+  console.log('Probably not a central European country :D');
+}
+
+neighbours[neighbours.indexOf('Sweden')] = 'Republic of Sweden;';
+console.log(neighbours);
